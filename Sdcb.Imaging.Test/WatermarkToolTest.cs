c3c01c6ccf2b3106528e7ddc4ff029e98b5afff7
sdcb <sdflysha@qq.com>
2019-07-25 22:21:25 +0800

    update unit test

--- Sdcb.Imaging.Test/WatermarkToolTest.cs
+++ Sdcb.Imaging.Test/WatermarkToolTest.cs
@@ -9,13 +9,19 @@ namespace Sdcb.Imaging.Test
         [Fact]
         public void Test1()
         {
-            WatermarkTool.WatermarkText(File.OpenRead(@"D:\_\WatermarkDemo.png"), File.OpenWrite(@"D:\_\Demo2.png"), "水印在此");
+            WatermarkTool.WatermarkText(File.OpenRead(@"D:\_\WatermarkDemo.png"), File.OpenWrite(@"D:\_\Demo2.png"), "水印在此", 
+                font: "微软雅黑", 
+                fontSize: 30, 
+                colorARGB: 0x7f_FF_FF_FF);
         }
 
         [Fact]
         public void Test2()
         {
-            var ms = WatermarkTool.WatermarkText(File.OpenRead(@"D:\_\WatermarkDemo.png"), "水印在此");
+            var ms = WatermarkTool.WatermarkText(File.OpenRead(@"D:\_\WatermarkDemo.png"), "水印在此",
+                font: "微软雅黑",
+                fontSize: 30,
+                colorARGB: 0x7f_FF_FF_FF);
             using (var file = File.OpenWrite(@"D:\_\Demo2.png"))
             {
                 ms.CopyTo(file);
