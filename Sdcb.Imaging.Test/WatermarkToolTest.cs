3052c7a7954dc51896c25387135ccfc68657b10f
sdcb <sdflysha@qq.com>
2019-08-12 08:55:50 +0800

    add ImageFormat support.

--- Sdcb.Imaging.Test/WatermarkToolTest.cs
+++ Sdcb.Imaging.Test/WatermarkToolTest.cs
@@ -15,6 +15,7 @@ namespace Sdcb.Imaging.Test
                 WatermarkTool.WatermarkText(
                     imageStream, outputStream,
                     watermark: "Watermark here",
+                    ImageFormats.Png, 
                     font: "Times New Roman",
                     fontSize: 30,
                     colorARGB: 0x7f_FF_FF_FF); // transparent white
@@ -27,10 +28,24 @@ namespace Sdcb.Imaging.Test
             var bytes = WatermarkTool.WatermarkText(
                 imageStream: File.OpenRead(@"..\..\..\..\sample-images\src.png"), 
                 watermark:  "Watermark here",
+                srcImageFormat: ImageFormats.Png,
                 font:       "Times New Roman",
                 fontSize:    30,
                 colorARGB:   0x7f_FF_FF_FF); // transparent white
             File.WriteAllBytes(@"..\..\..\..\sample-images\watermark-test2.png", bytes);
+        }
+
+        [Fact]
+        public void JpegTest()
+        {
+            var bytes = WatermarkTool.WatermarkText(
+                imageStream: File.OpenRead(@"..\..\..\..\sample-images\src.jpg"),
+                watermark: "Watermark here",
+                srcImageFormat: ImageFormats.Jpeg,
+                font: "Times New Roman",
+                fontSize: 30,
+                colorARGB: 0x7f_FF_FF_FF); // transparent white
+            File.WriteAllBytes(@"..\..\..\..\sample-images\watermark-dest.jpg", bytes);
         }
     }
 }
--- Sdcb.Imaging/WatermarkTool.cs
+++ Sdcb.Imaging/WatermarkTool.cs
@@ -3,6 +3,8 @@ using WIC = SharpDX.WIC;
 using D2D = SharpDX.Direct2D1;
 using DWrite = SharpDX.DirectWrite;
 using SharpDX;
+using System;
+using System.Collections.Generic;
 
 namespace Sdcb.Imaging
 {
@@ -10,11 +12,14 @@ namespace Sdcb.Imaging
     {
         public const int TransparentWhite = 0x7FFFFFFF;
 
-        public static void WatermarkText(Stream imageStream, Stream outputStream, string watermark, string font = "Times New Roman", float fontSize = 30.0f, int colorARGB = TransparentWhite)
+        public static void WatermarkText(Stream imageStream, Stream outputStream, string watermark,
+            ImageFormats srcImageFormat, ImageFormats? destImageFormat = null, string font = "Times New Roman", float fontSize = 30.0f, int colorARGB = TransparentWhite)
         {
+            Guid srcImageFormatGuid = ImageFormatGuidMapping[srcImageFormat];
+            Guid destImageFormatGuid = ImageFormatGuidMapping[destImageFormat ?? srcImageFormat];
             using (var wic = new WIC.ImagingFactory2())
             using (var d2d = new D2D.Factory())
-            using (var image = CreateWicImage(wic, imageStream))
+            using (var image = CreateWicImage(wic, imageStream, srcImageFormatGuid))
             using (var wicBitmap = new WIC.Bitmap(wic, image.Size.Width, image.Size.Height, WIC.PixelFormat.Format32bppPBGRA, WIC.BitmapCreateCacheOption.CacheOnDemand))
             using (var target = new D2D.WicRenderTarget(d2d, wicBitmap, new D2D.RenderTargetProperties()))
             using (var bmpPicture = D2D.Bitmap.FromWicBitmap(target, image))
@@ -34,20 +39,21 @@ namespace Sdcb.Imaging
                 }
                 target.EndDraw();
 
-                SaveD2DBitmap(wic, wicBitmap, outputStream);
+                SaveD2DBitmap(wic, wicBitmap, outputStream, destImageFormatGuid);
             }
         }
 
-        public static byte[] WatermarkText(Stream imageStream, string watermark, string font = "Times New Roman", float fontSize = 30.0f, int colorARGB = TransparentWhite)
+        public static byte[] WatermarkText(Stream imageStream, string watermark, ImageFormats srcImageFormat, ImageFormats? destImageFormat = null,
+            string font = "Times New Roman", float fontSize = 30.0f, int colorARGB = TransparentWhite)
         {
             var ms = new MemoryStream();
-            WatermarkText(imageStream, ms, watermark, font, fontSize, colorARGB);
+            WatermarkText(imageStream, ms, watermark, srcImageFormat, destImageFormat, font, fontSize, colorARGB);
             return ms.ToArray();
         }
 
-        private static void SaveD2DBitmap(WIC.ImagingFactory2 wic, WIC.Bitmap wicBitmap, Stream outputStream)
+        private static void SaveD2DBitmap(WIC.ImagingFactory2 wic, WIC.Bitmap wicBitmap, Stream outputStream, Guid imageFormatGuid)
         {
-            using (var encoder = new WIC.BitmapEncoder(wic, WIC.ContainerFormatGuids.Png))
+            using (var encoder = new WIC.BitmapEncoder(wic, imageFormatGuid))
             {
                 encoder.Initialize(outputStream);
                 using (var frame = new WIC.BitmapFrameEncode(encoder))
@@ -65,9 +71,9 @@ namespace Sdcb.Imaging
             }
         }
 
-        private static WIC.FormatConverter CreateWicImage(WIC.ImagingFactory2 wic, Stream imageStream)
+        private static WIC.FormatConverter CreateWicImage(WIC.ImagingFactory2 wic, Stream imageStream, Guid imageFormatGuid)
         {
-            using (var decoder = new WIC.PngBitmapDecoder(wic))
+            using (var decoder = new WIC.BitmapDecoder(wic, imageFormatGuid))
             {
                 var decodeStream = new WIC.WICStream(wic, imageStream);
                 decoder.Initialize(decodeStream, WIC.DecodeOptions.CacheOnLoad);
@@ -79,5 +85,31 @@ namespace Sdcb.Imaging
                 }
             }
         }
+
+        public static Dictionary<ImageFormats, Guid> ImageFormatGuidMapping = new Dictionary<ImageFormats, Guid>
+        {
+            [ImageFormats.Bmp] = WIC.ContainerFormatGuids.Bmp,
+            [ImageFormats.Png] = WIC.ContainerFormatGuids.Png,
+            [ImageFormats.Ico] = WIC.ContainerFormatGuids.Ico,
+            [ImageFormats.Jpeg] = WIC.ContainerFormatGuids.Jpeg,
+            [ImageFormats.Tiff] = WIC.ContainerFormatGuids.Tiff,
+            [ImageFormats.Gif] = WIC.ContainerFormatGuids.Gif,
+            [ImageFormats.Wmp] = WIC.ContainerFormatGuids.Wmp,
+            [ImageFormats.Dds] = WIC.ContainerFormatGuids.Dds,
+            [ImageFormats.Adng] = WIC.ContainerFormatGuids.Adng,
+        };
+    }
+
+    public enum ImageFormats
+    {
+        Bmp,
+        Png,
+        Ico,
+        Jpeg,
+        Tiff,
+        Gif,
+        Wmp,
+        Dds,
+        Adng,
     }
 }
